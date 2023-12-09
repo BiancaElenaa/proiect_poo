dfbe66e3389aa1a8ffbda8907671765cbfc7c5c4
BiancaElenaa <153358335+BiancaElenaa@users.noreply.github.com>
2023-12-09 13:55:53 +0200

    Update [proiect][1052][Bucur Bianca Elena].cpp

--- [proiect][1052][Bucur Bianca Elena].cpp
+++ [proiect][1052][Bucur Bianca Elena].cpp
@@ -1,68 +1,81 @@
 // [proiect][1052][Bucur Bianca Elena].cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <string.h>
 
 using namespace std;
 //prima clasa
 class ingredient
 {
-	const string* denumireIngredient;
+	const string* denumireIngrediente;
 	float* cantitatea_disponibila;
 
 public:
-	
+
 	//destructorul clasei
 	~ingredient()
 	{
-		delete denumireIngredient;
+		delete denumireIngrediente;
 		delete cantitatea_disponibila;
 	}
-	
+
 	//constructor
-	ingredient(const string &nume, float cantitate) : denumireIngredient(new string (nume)), cantitatea_disponibila(new float (cantitate)){}
-	ingredient(): denumireIngredient(nullptr),cantitatea_disponibila(nullptr)
+	ingredient(const string* denumireIngrediente, float* cantitatea_disponibila,int nrIng) 
+	{
+		this->denumireIngrediente = new string[nrIng];
+		this->denumireIngrediente = denumireIngrediente;
+	this->cantitatea_disponibila = cantitatea_disponibila;
+
+	
+	}
+	ingredient() : denumireIngrediente(nullptr), cantitatea_disponibila(nullptr)
 	{}
 };
 
 //a doua clasa
 class preparat
 {
-	string* denumire_preparat;
-	static int numar_total_preparate;
-	ingredient* lista_ingrediente;
+	string denumire_preparat;
+	static int numar_ordine_preparat;
+	//int numar_ingrediente;
+	ingredient** lista_ingrediente;
 	float* cantitatile_necesare;
 public:
 	//destructorul clasei
 	~preparat()
 	{
-		delete denumire_preparat;
+		//delete denumire_preparat;
 		delete[] lista_ingrediente;
 		delete cantitatile_necesare;
 	}
 
 	//constructor 
-	preparat(const string& denumire,const string& lista,float cantitate,int nr) : denumire_preparat(new string (denumire)), lista_ingrediente(new ingredient[nr]), cantitatile_necesare(new float (cantitate))
-	{
+	preparat() :denumire_preparat(nullptr), lista_ingrediente(nullptr), cantitatile_necesare(nullptr) {}
+	preparat(const string& denumire_preparat, ingredient** lista_ingrediente, float cantitatile_necesare, int numar_ingrediente) {
+		this->denumire_preparat = denumire_preparat;
+		if(numar_ingrediente>0 && lista_ingrediente!=nullptr)
+		{
+			this->lista_ingrediente = new ingredient*[numar_ingrediente];
+			for (int i = 0; i < numar_ingrediente; i++)
+				this->lista_ingrediente[i] = new ingredient(*lista_ingrediente[i]);
+		}
 		
-			cout << "numele preparatului :" << *denumire_preparat << "\n ingredientele sale:";
-			if (lista_ingrediente != nullptr)
+
+		/*cout << "numele preparatului :" << *denumire_preparat << "\n ingredientele sale:";
+		if (lista_ingrediente != nullptr)
 			cout << (lista_ingrediente[1].denumireIngredient) << " ";
-
-
-		
+		*/
 	}
-	preparat():denumire_preparat(nullptr),lista_ingrediente(nullptr),cantitatile_necesare(nullptr){}
-	
 };
-int preparat::numar_total_preparate = 0;
+int preparat::numar_ordine_preparat = 1;
 
 //a treia clasa
 class meniu
 {
 	preparat* denumirile_preparatelor;
-	 int numar_preparate_disponibile;
+	int numar_preparate_disponibile;
 public:
 	//destructorul clasei
 	~meniu()
@@ -70,14 +83,18 @@ public:
 		delete[] denumirile_preparatelor;
 	}
 	//constructor
-	meniu(const string& denum, int nr) :denumirile_preparatelor(new preparat[nr]), numar_preparate_disponibile(nr) {}
-	
+	meniu(preparat*denumirile_preparatelor,int numar_preparate_disponibile)
+	{
+		this->denumirile_preparatelor = denumirile_preparatelor;
+		this->numar_preparate_disponibile = numar_preparate_disponibile;
+	}
+
 };
 
 //a patra clasa
 class comanda
 {
-	string* lista_produse_comandate;
+	char* lista_produse_comandate;
 	int numar_preparate_comandate;
 public:
 	//destructorul clasei
@@ -85,32 +102,39 @@ public:
 	{
 		delete[] lista_produse_comandate;
 	}
-	comanda(const string& list, int nr) :lista_produse_comandate(new string (list)), numar_preparate_comandate(nr)
-	{}
+	comanda(char* lista_produse_comandate, int numar_preparate_comandate)
+	{
+		strcpy(this->lista_produse_comandate, lista_produse_comandate);
+		this->numar_preparate_comandate = numar_preparate_comandate;
+	}
 
 };
 
 //a cincea clasa
 class ospatar
 {
-	string* nume_ospatar;
-	bool* comanda_a_fost_preluata;
+	char* nume_ospatar = nullptr;
+	int* id_comenzi = { 0 };
 public:
 	//destructorul clasei
 	~ospatar()
 	{
-		delete nume_ospatar;
-		delete comanda_a_fost_preluata;
+		
+		delete id_comenzi;
 	}
-	ospatar(const string& nume, bool preluare):nume_ospatar(new string(nume)), comanda_a_fost_preluata(new bool(preluare))
-	{}
+	ospatar	(){}
+	ospatar( char* nume_ospatar, int * id_comenzi)
+	{
+		strcpy(this->nume_ospatar, nume_ospatar);
+		id_comenzi = nullptr;
+	}
 };
 
 
 int main()
 {
-	ingredient clatiteing(("oua","lapte","faina"), (30.0,100.0,25.0));
-	preparat clatite("clatitee", ("oua", "lapte", "faina"), (12, 23, 10),3);
+	ingredient clatiteing(( "oua", "lapte", "faina" ), ( 30.0, 100.0, 25.0 ), 3);
+	//preparat clatite("clatitee", ("oua", "lapte", "faina"), (12, 23, 10), 3);
 	//clatite.afis();
 	return 0;
 }
